d668dac16ba4eca80a3dc509c8db062cd24a565d
JoshAubrey <joshaubrey7@gmail.com>
2020-10-02 15:03:41 -0500

    refactor project structure to node.js best practices

--- index.js
+++ index.js
@@ -1,53 +1,63 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
+// if (process.env.NODE_ENV !== 'production') {
+//     require('dotenv').config()
+// }
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+//const express = require('express')
+//const app = express()
+//const cors = require('cors')
+//const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
+// const blogSchema = new mongoose.Schema({
+//   title: String,
+//   author: String,
+//   url: String,
+//   likes: Number
+// })
 
-const mongoUrl = process.env.MONGODB_URI
-console.log('connecting to', mongoUrl)
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+// const Blog = mongoose.model('Blog', blogSchema)
 
-app.use(cors())
-app.use(express.json())
+// const mongoUrl = process.env.MONGODB_URI
+// console.log('connecting to', mongoUrl)
+// mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+//     .then(result => {
+//         console.log('connected to MongoDB')
+//     })
+//     .catch((error) => {
+//         console.log('error connecting to MongoDB:', error.message)
+//     })
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+// app.use(cors())
+// app.use(express.json())
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+// app.get('/api/blogs', (request, response) => {
+//   Blog
+//     .find({})
+//     .then(blogs => {
+//       response.json(blogs)
+//     })
+// })
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+// app.post('/api/blogs', (request, response) => {
+//   const blog = new Blog(request.body)
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+//   blog
+//     .save()
+//     .then(result => {
+//       response.status(201).json(result)
+//     })
+// })
+
+// const PORT = process.env.PORT
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`)
+// })
+
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
