1c3e62a5e0359f54ac405a8506d0c0425630fca5
JoshAubrey <joshaubrey7@gmail.com>
2020-10-15 12:53:03 -0500

    create user model, controller, and tests and login controller

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,11 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response, next) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({})
+    .populate('user', {username: 1, name: 1})
   response.json(blogs)
 })
 
@@ -18,15 +21,20 @@ blogRouter.get('/:id', async (request, response, next) => {
 blogRouter.post('/', async (request, response, next) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
+    user: user._id,
   })
 
   try { 
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
     response.status(201).json(savedBlog)
   } catch(exception) {
     next(exception)
